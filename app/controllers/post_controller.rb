67371b299a64adcb49e861899cd9543cb2d0c7eb
EC2 Default User <ec2-user@ip-172-31-29-124.us-east-2.compute.internal>
2019-12-23 07:08:23 +0000

    新規投稿

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,12 @@
+class PostController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  def new
+  end
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/post/index")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'post/index' => 'post#index'
+  get 'post/new' => 'post#new'
+  post 'post/create' => 'post#create'
+
+  get '/' => 'home#top'
+  get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
